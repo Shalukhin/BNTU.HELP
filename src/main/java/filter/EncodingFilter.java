fe45b12ae7b4f3d3ab40a578a9276d94d32e7f5f
Vasiliy <mobil5662016@gmail.com>
2020-03-25 16:36:13 +0300

    Add filter encoding UTF-8

---
+++ src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,40 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+	
+	String encoding;
+   
+    public EncodingFilter() {
+    }
+    
+    public void init(FilterConfig fConfig) throws ServletException {
+		encoding = fConfig.getInitParameter("encoding");
+	}
+
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		if (encoding != null) {
+			if (!encoding.equalsIgnoreCase(request.getCharacterEncoding())) {
+				request.setCharacterEncoding(encoding);
+			}
+			if (!encoding.equalsIgnoreCase(response.getCharacterEncoding())) {
+				response.setCharacterEncoding(encoding);
+			}
+		}		
+		
+		chain.doFilter(request, response);
+	}	
+	
+	public void destroy() {
+	}
+
+}
